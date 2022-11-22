d32f441b8275f6d3ed55ff8a38d6abb1ff1d24ab
Austin <pizzahi5@gmail.com>
2022-11-21 18:17:49 -0800

    Starting_to_write_unit_tests

--- test/BootcampHW19.t.sol
+++ test/BootcampHW19.t.sol
@@ -3,14 +3,18 @@ pragma solidity ^0.8.0;
 
 import "forge-std/Test.sol";
 import "src/BootcampHW19.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract ShameCoinTest is Test {
+    using Strings for address;
 
     ShameCoin public eg;
 
-    function setUp(string memory _name, string memory _symbol) public {
-        eg = new ShameCoin(_name, _symbol, address(this));
-        emit log(string(abi.encodePacked("ShameCoin owner is ", address(this))));
+    function setUp() public {
+        eg = new ShameCoin("Shame", "SHM", address(this));
+        // Put effort into learning how to log with Foundry
+        emit log(string(abi.encodePacked("ShameCoin owner is ", address(this).toHexString())));
+        emit log(string(abi.encodePacked("ShameCoin address is ", address(this).toHexString())));
     }
 
     function testMint() public {
