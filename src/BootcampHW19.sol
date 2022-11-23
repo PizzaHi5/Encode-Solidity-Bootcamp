22c2f2c58f402c7d518b497040bf77b1d744e7c8
Austin <pizzahi5@gmail.com>
2022-11-23 15:53:15 -0800

    Finished_hw19

--- src/BootcampHW19.sol
+++ src/BootcampHW19.sol
@@ -5,13 +5,19 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+/// @title This contract creates ShameTokens distrubuted by an admin
+/// @author Austin (PizzaHi5)
+
+/// @notice Give admin control to a trusted entity for distributing shame coins
+/// @custom:experimental This is an experimental contract
+
 contract ShameCoin is ERC20, Ownable {
 
-    mapping(address => bool) private canAdminTransfer;
-    mapping(address => bool) private canAdminBurn;
+    /// @dev These are boolean conditions for restricting control of the admin and minting
+    mapping(address => bool) public canAdminTransfer;
+    mapping(address => bool) public canAdminBurn;
+    mapping(address => bool) public isMinting;
 
-    //Put a mempool tracker as admin that mints ShameCoins to
-    //sandwich/frontrunners/backrunners ;)
     constructor(
     string memory _name, 
     string memory _symbol,
@@ -32,22 +38,27 @@ contract ShameCoin is ERC20, Ownable {
         canAdminBurn[msg.sender] = true;
     }
 
+    /// @notice Allows owner to mint to any address
     function mint(address target) public onlyOwner {
+        isMinting[target] = true;
         _mint(target, 1);
     }
 
+    /// @notice Allows owner to burn to any address if that address owner permits
     function burn(address target) public onlyOwner {
         require(canAdminBurn[target], "Not allowed to burn");
         canAdminBurn[target] = false;
         _burn(target, 1);
     }
 
+    /// @return bool: returns true if transfer succeeded
     function transfer(address to, uint256 amount) public virtual override returns (bool) {
         amount = 1;
         address sender = _msgSender();
         return checkIfOwner(sender) ? super.transfer(to, amount) : super.transfer(sender, amount);
     }
 
+    /// @return bool: returns true if transferFrom succeeded
     function transferFrom(address from, address to, uint256 amount) public virtual override onlyOwner returns (bool) {
         require(canAdminTransfer[from], "Not allowed to transfer");
         canAdminTransfer[from] = false;
@@ -55,9 +66,10 @@ contract ShameCoin is ERC20, Ownable {
         return super.transferFrom(from, to, amount);
     }
 
-    //This takes advantage of the hooks in ERC20
+    /// @dev Mints a token to non-owner transfers
     function _beforeTokenTransfer(address from, address to, uint256 amount) internal override {
-        if(!checkIfOwner(from)) {
+        if(!checkIfOwner(msg.sender) && isMinting[to]) {
+            isMinting[to] = false;
             _mint(to, amount);
         }
     }
--- test/BootcampHW19.t.sol
+++ test/BootcampHW19.t.sol
@@ -7,23 +7,45 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract ShameCoinTest is Test {
     using Strings for address;
+    using Strings for uint256;
 
     ShameCoin public eg;
+    address dummy = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
 
     function setUp() public {
         eg = new ShameCoin("Shame", "SHM", address(this));
-        // Put effort into learning how to log with Foundry
-        emit log(string(abi.encodePacked("ShameCoin owner is ", address(this).toHexString())));
-        emit log(string(abi.encodePacked("ShameCoin address is ", address(eg).toHexString())));
+        //emit log(string(abi.encodePacked("ShameCoin owner is ", address(this).toHexString())));
+        //emit log(string(abi.encodePacked("ShameCoin address is ", address(eg).toHexString())));
     }
 
     function testMint() public {
         eg.mint(msg.sender);
         assertEq(eg.balanceOf(msg.sender), 1);
-        emit log_uint(eg.balanceOf(msg.sender));
+        emit log(string(abi.encodePacked("Number of Tokens Minted: ", eg.balanceOf(msg.sender).toString())));
     }
 
     function testburn() public {
-        
+        //emit log(string(abi.encodePacked("Dummy address is ", dummy.toHexString())));
+        eg.mint(dummy);
+        emit log(string(abi.encodePacked("Dummy Address Tokens: ", eg.balanceOf(dummy).toString())));
+
+        startHoax(dummy);
+        eg.allowAdminBurn();
+        assertTrue(eg.canAdminBurn(dummy));
+        vm.stopPrank();
+        eg.burn(dummy);
+
+        emit log(string(abi.encodePacked("Dummy Address Tokens: ", eg.balanceOf(dummy).toString())));
+    }
+
+    function testTransfer() public {
+        eg.mint(dummy);
+        emit log(string(abi.encodePacked("Dummy Address Tokens: ", eg.balanceOf(dummy).toString())));
+        startHoax(dummy);
+        eg.transfer(address(eg), 100);
+        assertEq(eg.balanceOf(dummy), 2);
+        assertEq(eg.balanceOf(address(eg)), 0);
+        emit log(string(abi.encodePacked("Dummy Address Tokens: ", eg.balanceOf(dummy).toString())));
+        emit log(string(abi.encodePacked("Target Address Tokens: ", eg.balanceOf(address(eg)).toString())));
     }
 }
