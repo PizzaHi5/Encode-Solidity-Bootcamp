5ade7a1255427075283be581b46d4bfc629ac9d4
Austin <pizzahi5@gmail.com>
2022-11-27 13:10:57 -0800

    Continueing_Hw20

--- test/BootcampHw20.t.sol
+++ test/BootcampHw20.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 
 import "forge-std/Test.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "src/interfaces/ISwapRouter.sol";
 
 contract UniswapTest is Test {
 
@@ -10,18 +11,6 @@ contract UniswapTest is Test {
     address dai = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     address busd = 0xDFd5293D8e347dFe59E90eFd55b2956a1343963d;
     address usdc = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-
-    /// @dev copied from ISwapRouter
-    struct ExactInputSingleParams {
-        address tokenIn;
-        address tokenOut;
-        uint24 fee;
-        address recipient;
-        uint256 deadline;
-        uint256 amountIn;
-        uint256 amountOutMinimum;
-        uint160 sqrtPriceLimitX96;
-    }
 
     function setUp() public {
 
